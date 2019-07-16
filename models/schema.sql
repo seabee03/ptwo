49f1d48fb861517bd0f3f06f90636cd0e3e9462c
seabee03 <darcus.prado@gmail.com>
2019-07-16 11:46:51 -0500

    added database to schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,21 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+-- Drops the dogspawdb if it exists currently --
+DROP DATABASE IF EXISTS dogspawdb;
+-- Creates the "dogspawdb" database --
+CREATE DATABASE dogspawdb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- Makes it so all of the following code will affect dogspawdb --
+USE dogspawdb;
+
+-- Creates the table "appt_request" within dogspawdb --
+CREATE TABLE appt_request (
+
+-- Creates columns in table below
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  pet_name VARCHAR(100) NOT NULL,
+  pet_breed VARCHAR(100) NOT NULL,
+  pet_age INTEGER(2),
+  pet_weight INTEGER(3),
+  rabies_vaccination BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
+
