f9bb2328957717654e9a2b3aa706e51731a41e43
seabee03 <darcus.prado@gmail.com>
2019-07-18 12:05:19 -0500

    changed my db name back to doggyspawdb and corrected my config info

--- models/schema.sql
+++ models/schema.sql
@@ -1,11 +1,11 @@
 -- Drops the dogspawdb if it exists currently --
-DROP DATABASE IF EXISTS o4uecp5rn6ya68qw;
+DROP DATABASE IF EXISTS dogspawdb;
 
 -- Creates the "dogspawdb" database --
-CREATE DATABASE o4uecp5rn6ya68qw;
+CREATE DATABASE dogspawdb;
 
 -- Makes it so all of the following code will affect dogspawdb --
-USE o4uecp5rn6ya68qw;
+USE dogspawdb;
 
 -- Creates the table "appt_request" within dogspawdb --
 CREATE TABLE appt_request (
