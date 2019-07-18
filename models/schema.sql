750f620f42559709fe4e950ca1d4439f5a4062f7
Amanda Lowell <amandalowell2016@gmail.com>
2019-07-18 13:49:45 -0500

    nav cosmetics

--- models/schema.sql
+++ models/schema.sql
@@ -1,35 +1,20 @@
 -- Drops the dogspawdb if it exists currently --
-DROP DATABASE IF EXISTS o4uecp5rn6ya68qw;
-
+DROP DATABASE IF EXISTS dogspawdb;
 -- Creates the "dogspawdb" database --
-CREATE DATABASE o4uecp5rn6ya68qw;
+CREATE DATABASE dogspawdb;
 
 -- Makes it so all of the following code will affect dogspawdb --
-USE o4uecp5rn6ya68qw;
+USE dogspawdb;
 
 -- Creates the table "appt_request" within dogspawdb --
 CREATE TABLE appt_request (
 
--- Creates columns in table below --
+-- Creates columns in table below
   id INTEGER NOT NULL AUTO_INCREMENT,
   pet_name VARCHAR(100) NOT NULL,
   pet_breed VARCHAR(100) NOT NULL,
-  pet_age INTEGER(2) NOT NULL,
-  pet_weight INTEGER(3) NOT NULL,
+  pet_age INTEGER(2),
+  pet_weight INTEGER(3),
   rabies_vaccination BOOLEAN DEFAULT false,
-  
-  -- Services that can be chosen --
-  14_step_spaw BOOLEAN DEFAULT false,
-  
-  -- Additional Services that can be added onto the 14 step spaw service --
-  de_shedding BOOLEAN DEFAULT false,
-  pad_treatment BOOLEAN DEFAULT false,
-  shave_down BOOLEAN DEFAULT false,
-  clips BOOLEAN DEFAULT false,
-  flee_tick BOOLEAN DEFAULT false,
-  teeth_brush BOOLEAN DEFAULT false,
-  pawdicure BOOLEAN DEFAULT false,
-  hair_color BOOLEAN DEFAULT false,
   PRIMARY KEY(id)
 );
-
