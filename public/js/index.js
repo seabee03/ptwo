d0b994b5499ce59366de2e62c8bd94fe7d308d07
seabee03 <darcus.prado@gmail.com>
2019-07-19 10:43:23 -0500

    commented out the services in all files

--- public/js/index.js
+++ public/js/index.js
@@ -4,17 +4,17 @@ var $appointmentPetBreed = $("#appointment-pet_breed");
 var $appointmentPetAge = $("#appointment-pet_age");
 var $appointmentPetWeight = $("#appointment-pet_weight");
 var $appointmentRabiesVaccination = $("#appointment-rabies_vaccination");
-var $appointment14StepSpaw = $("#appointment-14_step_spaw");
-var $appointmentDeShedding= $("#appointment-de_shedding");
-var $appointmentPadTreatment= $("#appointment-pad_treatment");
-var $appointmentShaveDown= $("#appointment-shave_down");
-var $appointmentClips= $("#appointment-clips");
-var $appointmentFleeTick= $("#appointment-flee_tick");
-var $appointmentTeethBrush= $("#appointment-teeth_brush");
-var $appointmentPawdicure= $("#appointment-pawdicure");
-var $appointmentHairColor= $("#appointment-hair_color");
-var $submitBtn = $("#submit");
-var $appointmentList = $("#appointment-list");
+// var $appointment14StepSpaw = $("#appointment-14_step_spaw");
+// var $appointmentDeShedding= $("#appointment-de_shedding");
+// var $appointmentPadTreatment= $("#appointment-pad_treatment");
+// var $appointmentShaveDown= $("#appointment-shave_down");
+// var $appointmentClips= $("#appointment-clips");
+// var $appointmentFleeTick= $("#appointment-flee_tick");
+// var $appointmentTeethBrush= $("#appointment-teeth_brush");
+// var $appointmentPawdicure= $("#appointment-pawdicure");
+// var $appointmentHairColor= $("#appointment-hair_color");
+// var $submitBtn = $("#submit");
+// var $appointmentList = $("#appointment-list");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
@@ -78,20 +78,20 @@ var handleFormSubmit = function(event) {
 
   var appointment = {
     // Get references to page elements
-    pet name: $appointmentPetName.val().trim(),
-    pet breed: $appointmentPetBreed.val().trim(),
-    pet age: $appointmentPetAge.val().trim(),
-    pet weight: $appointmentPetWeight.val().trim(),
-    rabies vaccination: $appointmentRabiesVaccination.val().trim(),
-    14 step spaw: $appointment14StepSpaw.val().trim(),
-    desheeding: $appointmentDeShedding.val().trim(),
-    pad treatment: $appointmentPadTreatment.val().trim(),
-    shave down: $appointmentShaveDown.val().trim(),
-    clips: $appointmentClips.val().trim(),
-    flee and tick: $appointmentFleeTick.val().trim(),
-    teeth brushing: $appointmentTeethBrush.val().trim(),
-    pawdicure: $appointmentPawdicure.val().trim(),
-    hair color: $appointmentHairColor.val().trim()
+    petname: $appointmentPetName.val().trim(),
+    petbreed: $appointmentPetBreed.val().trim(),
+    petage: $appointmentPetAge.val().trim(),
+    petweight: $appointmentPetWeight.val().trim(),
+    rabiesvaccination: $appointmentRabiesVaccination.val().trim(),
+    // 14stepspaw: $appointment14StepSpaw.val().trim(),
+    // desheeding: $appointmentDeShedding.val().trim(),
+    // padtreatment: $appointmentPadTreatment.val().trim(),
+    // shavedown: $appointmentShaveDown.val().trim(),
+    // clips: $appointmentClips.val().trim(),
+    // fleeandtick: $appointmentFleeTick.val().trim(),
+    // teethbrushing: $appointmentTeethBrush.val().trim(),
+    // pawdicure: $appointmentPawdicure.val().trim(),
+    // haircolor: $appointmentHairColor.val().trim()
   };
 
   if (!(appointment.petname && appointment.petbreed)) {
@@ -102,23 +102,20 @@ var handleFormSubmit = function(event) {
   API.saveAppointment(appointment).then(function() {
     refreshAppointments();
   });
-
-  $appointmentText.val("");
-  $appointmentDescription.val("");
-  $appointmentPetName.val("");
-  $appointmentPetBreed.val("");
-  $appointmentPetAge.val("");
-  $appointmentPetWeight.val("");
-  $appointmentRabiesVaccination.val("");
-  $appointment14StepSpaw.val("");
-  $appointmentDeShedding.val("");
-  $appointmentPadTreatment.val("");
-  $appointmentShaveDown.val("");
-  $appointmentClips.val("");
-  $appointmentFleeTick.val("");
-  $appointmentTeethBrush.val("");
-  $appointmentPawdicure.val("");
-  $appointmentHairColor.val("");
+    $appointmentPetName.val("");
+    $appointmentPetBreed.val("");
+    $appointmentPetAge.val("");
+    $appointmentPetWeight.val("");
+    $appointmentRabiesVaccination.val("");
+    // $appointment14StepSpaw.val("");
+    // $appointmentDeShedding.val("");
+    // $appointmentPadTreatment.val("");
+    // $appointmentShaveDown.val("");
+    // $appointmentClips.val("");
+    // $appointmentFleeTick.val("");
+    // $appointmentTeethBrush.val("");
+    // $appointmentPawdicure.val("");
+    // $appointmentHairColor.val("");
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
